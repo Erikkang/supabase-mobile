c960e6e957b2848116d939380932e4d313bb2f1d
kitmac03 <jkriego03@gmail.com>
2026-04-18 22:47:43 +0800

    api updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -102,7 +102,7 @@ export default function HomeScreen() {
   const scaleAnim = useRef(new Animated.Value(0.95)).current;
   const infoHeightAnim = useRef(new Animated.Value(1)).current;
 
-  const API_BASE_URL = "https://facial-backend-92vm.onrender.com";
+  const API_BASE_URL = "https://kitmac03-facial-backend.hf.space/";
 
   useEffect(() => {
     Animated.parallel([
