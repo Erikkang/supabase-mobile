d6f8f7e69b45c6baed6c0728cca9ec3a1f8dbce8
kitmac03 <jkriego03@gmail.com>
2026-04-30 00:56:01 +0800

    error message updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -175,7 +175,7 @@ const API_BASE_URL = "https://kitmac03-facial-backend.hf.space";
       const allModelResults = await callBackendAPI(selectedImage);
       setResults(allModelResults);
     } catch (error: any) {
-      let errorMsg = 'Failed to analyze image. Please try again.';
+      let errorMsg = 'Unable to analyze this image. Please upload a clear, close-up photo of the affectec skin area in good lighting';
 
       if (error.message?.includes('Network') || error.message?.includes('fetch')) {
         errorMsg = 'Connection error. Check your network and try again.';
